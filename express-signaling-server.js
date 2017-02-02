d59fbb0715c0bee52b4fc0a000ab4f435f655a42
folkvir <dev.arnaudgrall@gmail.com>
2017-02-02 11:46:51 +0100

    fix a problem with signaling-server and change version

--- express-signaling-server.js
+++ express-signaling-server.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var express = require("express");
 var app = express();
-var signserver = require('./karma-signaling.js');
+var signserver = require('./signaling-server.js');
 
 app.use('/static', express.static(__dirname + "/"));
 
@@ -10,42 +10,3 @@ app.get('/', function(req, res){
 });
 
 signserver(app, console.log);
-
-// io.on('connection', function(socket){
-//   number++;
-// 	//log.info('A user is connected');
-// 	socket.on("joinRoom",function(room){
-// 		//log.info('A user join the room : ' + room);
-// 		//log.info(socket.id);
-// 		socket.join(room);
-// 	});
-// 	socket.on("new",function(data){
-// 				let room = data.room;
-// 				let offer = data.offer;
-// 				clients[data.offer.tid] = socket ;
-// 				//console.log("Emit the new offer on the room " + room + " for the socketId : " + socket.id);
-// 				socket.broadcast.in(room).emit("new_spray", offer);
-// 	});
-// 	socket.on("accept", function(data){
-// 		let room = data.room;
-// 		let offer = data.offer;
-//
-// 		//console.log("Server received an accepted ticket for " + socketId);
-// 		if(clients[data.offer.tid] != null){
-// 			//console.log(offer);
-// 			clients[data.offer.tid].emit("accept_spray", offer);
-// 		}
-// 		clients[data.offer.tid] = null;
-// 	});
-//
-// 	socket.on('disconnect', function(room, socketId){
-// 		//log.info('A user disconnected');
-// 		socket.leave(room);
-// 		number--;
-// 	});
-//
-// });
-//
-// http.listen(port, function () {
-//   console.log('HTTP Server listening on port '+port);
-// });
