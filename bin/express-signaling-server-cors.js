abb613817ab661940e184ba4fe799e32259cf392
Folkvir <folkvir@outlook.fr>
2018-05-16 17:07:41 +0200

    full cors, express+socket.io

--- bin/express-signaling-server-cors.js
+++ bin/express-signaling-server-cors.js
@@ -3,13 +3,15 @@ const express = require('express')
 const app = express()
 const signserver = require('../src/signaling-server.js')
 const cors = require('cors')
+const io = require('socket.io')
+const httpServer = require('http')
 
 const host = process.env.HOST || 'localhost'
 const port = parseInt(process.env.PORT) || 3000
 console.log('Host: ', host)
 console.log('Port: ', port)
 
-const iooptions = { origins: '*:*' }
+const iooptions = { origins: 'true' }
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -23,4 +25,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-signserver(app, console.log, host, port, iooptions)
+const ioServer = io(httpServer, iooptions)
+ioServer.origins((origin, callback) => {
+  callback(null, true)
+})
+
+signserver(app, console.log, host, port, ioServer, iooptions)
